d358fd985b9cb27b65d761cc774e7c7bf519e5c3
Aura Tao <auratao.model@gmail.com>
2025-01-15 18:07:04 +0100

    the first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from '../src/components/Footer.jsx';
+import Navbar from './components/NavBar.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = ()=>{
+    return (
+        <header className="bg-blue-500 text-white p-4">
+            <h1>我是Header</h1>
+        </header>
+    )
+}
+
+export default Navbar;
